5212022d876283b6f0541a5de68acd8be4843cfb
shevig22 <111096951+shevig22@users.noreply.github.com>
2023-10-23 18:32:34 -0500

    Add files via upload

---
+++ 114/test1.py
@@ -0,0 +1,6 @@
+a = 33
+b = 33
+if b > a:
+    print("b es mayor que a")
+elif a == b:
+    print("a y b son iguales")
---
+++ 114/test2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname + " refsnes")
+    
+my_function("emil")
+my_function("tobias")
+my_function("linus")
